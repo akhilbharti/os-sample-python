773888e571cf3f6c0d8d81bb84073ac7174195a5
akhilbharti <akhbharti@gmail.com>
2017-11-14 16:31:56 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "<iframe width="450" height="260" style="border: 1px solid #cccccc;" src="https://thingspeak.com/channels/351449/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&title=Temperature&type=spline&xaxis=Time&yaxis=Temperature&yaxismax=50&yaxismin=0"></iframe>"
+    return"hello world!!"
 
 if __name__ == "__main__":
     application.run()
